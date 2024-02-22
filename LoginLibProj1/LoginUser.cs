e87a0519fc448f768ecc998b7c8f03c741d78543
TatikondaAnvitha123 <tatikondaanvitha05@gmail.com>
2024-02-22 19:53:17 +0530

    Add project files.

---
+++ LoginLibProj1/LoginUser.cs
@@ -0,0 +1,21 @@
+﻿namespace LoginLibProj1
+{
+    public class LoginUser
+    {
+        public string Login(string username, string password)
+        {
+            string result = "";
+            if (username == "ram1256" && password == "ram@1256")
+            {
+                result = "Login Success";
+
+            }
+            else
+            {
+                result = "Login Failed";
+            }
+            return result;
+        }
+
+    }
+}
